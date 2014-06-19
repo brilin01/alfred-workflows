c5c445444351f0c2d43e4d42a9d7a03fd3c0eecd
Brian Lin <bri.lin01@gmail.com>
2014-06-18 21:52:24 -0400

    Updated revision date

--- placeholdit/src/placeholdit.php
+++ placeholdit/src/placeholdit.php
@@ -5,7 +5,7 @@
   * This PHP script generates a placehold.it image placeholder url and HTML code for Alfred.
   *
   * Version:  2.0.1
-  * Revised:  6/16/14
+  * Revised:  6/18/14
   * Author:   Brian Lin
   * Website:  https://github.com/brilin01/alfred-workflows
   */
